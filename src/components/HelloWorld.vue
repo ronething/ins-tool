53b31c05c14c56bf9eaf4c4372cf7ef9d7684d76
ronething <axingfly@gmail.com>
2019-04-14 02:02:16 +0800

    feat: 前端展示搭建

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <div class="title">
+          <h1>ins-tool</h1>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="16" :offset="4">
+        <div class="context">
+          <div class="label">
+            <label for="ins_url" style="float:left;padding-left:10px">
+              <h3>输入链接开始下载</h3>
+            </label>
+          </div>
+
+          <div class="ins">
+            <el-input id="ins_url" v-model="input" placeholder="请输入内容" class="ins_input"></el-input>
+            <el-button @click="getInsUrl()" class="int_btn" type="primary">获取链接</el-button>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row v-if="flag">
+      <el-col :span="16" :offset="4">
+        <div class="result">
+          <h3>获取成功</h3>
+          <div v-for="r in res">
+            <a :href="r">{{ r }}</a>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  props: {},
+  data() {
+    return {
+      input: "",
+      flag: false,
+      res: []
+    };
+  },
+  methods: {
+    getInsUrl() {
+      this.$message.warning("获取链接");
+      this.res = ["http://xxx.com", "http://bbb.com"];
+      this.flag = true;
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+h1,
+h3,
 a {
-  color: #42b983;
+  color: rgb(226, 137, 214);
+}
+.context {
+  height: 200px;
+  border: 2px solid rgb(226, 137, 214);
+}
+.result {
+  height: 200px;
+  border: 2px solid rgb(226, 137, 214);
+  margin-top: 30px;
+}
+.ins {
+}
+.ins_input {
+  display: inline-block;
+  width: 85%;
+}
+.ins_btn {
+  display: inline-block;
 }
 </style>
